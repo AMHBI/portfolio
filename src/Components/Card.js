c9e4094cb12ba0484ea501585196dc6848c7b8a1
AMHBI <134843068+AMHBI@users.noreply.github.com>
2023-08-14 01:04:00 +0330

    Complete Project Component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,20 @@ import React from 'react';
 import styles from "../Styles/Card.module.css";
 
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <div className={styles.imgWrapper}>
+                <img src={props.img} alt='project'/>
+            </div>
+
+            <div className={styles.textWrapper}>
+                <h3>{props.title}</h3>
+                <p>{props.desc}</p>
+                <button>
+                    <a href={props.url}>Visit {props.title}</a>
+                </button>
+            </div>
         </div>
     );
 };
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import Card from "./Card";
+import project from "../assets/jpg/project-1.jpg";
 import styles from "../Styles/Cards.module.css";
 const Cards = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [projects, setProjects] = useState([
+    {
+      img: project,
+      title: "Shop Landing",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ",
+      url: "https://amhb.ir"
+    },
+    {
+      img: project,
+      title: "Login",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ",
+      url: "https://amhb.ir"
+    },
+    {
+      img: project,
+      title: "Silver Home Shop",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ",
+      url: "https://amhb.ir"
+    },
+    {
+      img: project,
+      title: "silver",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ",
+      url: "https://amhb.ir"
+    },
+  ]);
+
+  return (
+    <div className={styles.container}>
+      {projects.map((pr) => {
+      return <Card img={pr.img} title={pr.title} desc={pr.desc} url={pr.url} />;
+      })}
+    </div>
+  );
 };
 
 export default Cards;
